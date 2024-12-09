e9c6760a30af9eca758af7518bdd6451b064bc89
Manish Yadav <manishyadav14127@gmail.com>
2024-12-09 20:49:13 +0530

    Initial Setup done

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+
+  return (
+    <>
+      <div className="m-3 p-2">
+        <Button>Codeon</Button>
+      </div>
+    </>
+  )
+}
+
+export default App;
